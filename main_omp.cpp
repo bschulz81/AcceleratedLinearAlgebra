191609786afeaaa89a4fa93ec10580392f120957
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2025-06-30 00:18:12 +0200

    Update main_omp.cpp
    updated to support the new version of the library

--- main_omp.cpp
+++ main_omp.cpp
@@ -28,7 +28,7 @@ size_t rows = 4, cols = 4;
 // Define some data for matrix multiplication A*B=C;
 vector<double>A_data(16,0);
 vector<double>B_data(16,0);
-vector<double>C_data(16,0);
+vector<double>C_data(16,1);
 
    // Allocate data
     for (size_t i = 0; i < rows * cols; ++i)
@@ -48,24 +48,32 @@ mdspan<double, std::vector<size_t>> C(C_data.data(), true, {rows, cols});
 //
 //simple matrix multiplication, if the boolean flag is set to true, the multiplication is done on gpu.
 cout<<"Ordinary matrix multiplication, on gpu"<<std::endl;
-matrix_multiply_dot(A, B, C,true);
+
+
+printmatrix(A);
+printmatrix(B);
+
+
+ matrix_multiply_dot(A, B, C,false,true);
+
 
 printmatrix(C);
 
 
 
 
+
 //Another set of data
-vector<double>A2_data={4,12,-16,12,37,-43,-16,-43,98};
-vector<double>L2_data(9,0);
-size_t rows2 = 3, cols2 = 3;
+    vector<double>A2_data={4,12,-16,12,37,-43,-16,-43,98};
+    vector<double>L2_data(9,0);
+    size_t rows2 = 3, cols2 = 3;
 
 
 //we want to do a cholesky decomposition with it
 
-mdspan<double, std::vector<size_t>> A2(A2_data.data(), true, {rows2, cols2});
-mdspan<double, std::vector<size_t>> L2(L2_data.data(), true, {rows2, cols2});
-cout<<"A Cholesky decomposition with the multiplication on gpu"<<std::endl;
+    mdspan<double, std::vector<size_t>> A2(A2_data.data(), true, {rows2, cols2});
+    mdspan<double, std::vector<size_t>> L2(L2_data.data(), true, {rows2, cols2});
+    cout<<"A Cholesky decomposition with the multiplication on gpu"<<std::endl;
 
 //the cholesky decomposition involves a matrix multiplication.
 //the parameter par can select a multiplication algorithm. by default it is the naive one.
@@ -78,97 +86,102 @@ cout<<"A Cholesky decomposition with the multiplication on gpu"<<std::endl;
 //per node and then use openmp and gpu on this device.
 
 // the step size (here set to 0, is a parameter from https://arxiv.org/abs/1812.02056. if it is zero, an optimal value for strassen's algorithm will be chosen)
-printmatrix(A2);
-matrix_multiplication_parameters par2;
-
-par2.gpu_offload=true;
-
-cholesky_decomposition(A2,L2,par2,0);
 
 
-printmatrix(L2);
+    printmatrix(A2);
+    matrix_multiplication_parameters par2;
+
+    par2.gpu_offload=true;
+
+    cholesky_decomposition(A2,L2,par2,0,false,false,0);
+
+
+    printmatrix(L2);
+
+
 //
 ////the first boolean flag implies that the entire cholesky decomposition (not just the multiplication) is now done on gpu
-cout<<"Now the cholesky decomposition is entirely done on gpu"<<std::endl;
+    cout<<"Now the cholesky decomposition is entirely done on gpu"<<std::endl;
 ////set the results to zero again.
-std::fill(L2_data.begin(),L2_data.end(),0);
+    std::fill(L2_data.begin(),L2_data.end(),0);
 
-cholesky_decomposition(A2,L2,par2,0,true);
+    cholesky_decomposition(A2,L2,par2,0,true);
 
-printmatrix(L2);
+    printmatrix(L2);
 
-cout<< "Now we do the same with the lu decomposition"<<std::endl;
-vector<double>A3_data={1,-2,-2,-3,3,-9,0,-9,-1,2,4,7,-3,-6,26,2};
-vector<double>L3_data(16,0);
-vector<double>U3_data(16,0);
-size_t rows3 = 4, cols3 = 4;
-mdspan<double, std::vector<size_t>> A3(A3_data.data(), true, {rows3, cols3});
-mdspan<double, std::vector<size_t>> L3(L3_data.data(), true, {rows3, cols3});
-mdspan<double, std::vector<size_t>> U3(U3_data.data(), true, {rows3, cols3});
+    cout<< "Now we do the same with the lu decomposition"<<std::endl;
+    vector<double>A3_data={1,-2,-2,-3,3,-9,0,-9,-1,2,4,7,-3,-6,26,2};
+    vector<double>L3_data(16,0);
+    vector<double>U3_data(16,0);
+    size_t rows3 = 4, cols3 = 4;
+    mdspan<double, std::vector<size_t>> A3(A3_data.data(), true, {rows3, cols3});
+    mdspan<double, std::vector<size_t>> L3(L3_data.data(), true, {rows3, cols3});
+    mdspan<double, std::vector<size_t>> U3(U3_data.data(), true, {rows3, cols3});
 
-matrix_multiplication_parameters par3;
-par3.gpu_offload=false;
-printmatrix(A3);
-cout<<"Just the multiplication on gpu"<<std::endl;
-lu_decomposition(A3,L3,U3,par3,0,false);
-printmatrix(L3);
-printmatrix(U3);
+    matrix_multiplication_parameters par3;
+    par3.gpu_offload=true;
+    printmatrix(A3);
+    cout<<"Just the multiplication on gpu"<<std::endl;
+    lu_decomposition(A3,L3,U3,par3,0,false);
+    printmatrix(L3);
+    printmatrix(U3);
 
-cout<<"Entirely on gpu"<<std::endl;
+    cout<<"Entirely on gpu"<<std::endl;
 
-std::fill(L3_data.begin(),L3_data.end(),0);
-std::fill(U3_data.begin(),U3_data.end(),0);
-lu_decomposition(A3,L3,U3,par3,0,true);
-printmatrix(L3);
-printmatrix(U3);
+    std::fill(L3_data.begin(),L3_data.end(),0);
+    std::fill(U3_data.begin(),U3_data.end(),0);
+    lu_decomposition(A3,L3,U3,par3,0,true);
+    printmatrix(L3);
+    printmatrix(U3);
 
+//
+    cout<< "Now we do the same with the qr decomposition"<<std::endl;
 
-cout<< "Now we do the same with the qr decomposition"<<std::endl;
+    vector<double>A4_data={12,-51,4,6,167,-68,-4,24,-41};
+    vector<double>Q4_data(9,0);
+    vector<double>R4_data(9,0);
+    size_t rows4 = 3, cols4 = 3;
 
-vector<double>A4_data={12,-51,4,6,167,-68,-4,24,-41};
-vector<double>Q4_data(9,0);
-vector<double>R4_data(9,0);
-size_t rows4 = 3, cols4 = 3;
+    mdspan<double, std::vector<size_t>> A4(A4_data.data(), true, {rows4, cols4});
+    mdspan<double, std::vector<size_t>> Q4(Q4_data.data(), true, {rows4, cols4});
+    mdspan<double, std::vector<size_t>> R4(R4_data.data(), true, {rows4, cols4});
+    matrix_multiplication_parameters par4;
+    printmatrix(A4);
 
-mdspan<double, std::vector<size_t>> A4(A4_data.data(), true, {rows4, cols4});
-mdspan<double, std::vector<size_t>> Q4(Q4_data.data(), true, {rows4, cols4});
-mdspan<double, std::vector<size_t>> R4(R4_data.data(), true, {rows4, cols4});
-matrix_multiplication_parameters par4;
-printmatrix(A4);
-
-cout<<"Just the multiplication on gpu"<<std::endl;
-par4.gpu_offload=false;
-qr_decomposition(A4,Q4,R4,par4,0,false);
-printmatrix(Q4);
-printmatrix(R4);
-
-
-std::fill(Q4_data.begin(),Q4_data.end(),0);
-std::fill(R4_data.begin(),R4_data.end(),0);
-
-cout<<"Entirely on gpu"<<std::endl;
-qr_decomposition(A4,Q4,R4,par4,0,true);
-printmatrix(Q4);
-printmatrix(R4);
+    cout<<"Just the multiplication on gpu"<<std::endl;
+    par4.gpu_offload=true;
+    qr_decomposition(A4,Q4,R4,par4,0,false);
+    printmatrix(Q4);
+    printmatrix(R4);
 
 
-cout<<"In order to test the qr decomposition, we can use Strassen's algorithm"<<endl;
+    std::fill(Q4_data.begin(),Q4_data.end(),0);
+    std::fill(R4_data.begin(),R4_data.end(),0);
 
-vector<double>C4_data(9,0);
-mdspan<double, std::vector<size_t>> C4(C4_data.data(), true, {rows4, cols4});
+    cout<<"Entirely on gpu"<<std::endl;
+    par4.gpu_offload=true;
+    qr_decomposition(A4,Q4,R4,par4,0,true);
+    printmatrix(Q4);
+    printmatrix(R4);
 
-strassen_multiply(Q4, R4, C4,par4);
+
+    cout<<"In order to test the qr decomposition, we can use Strassen's algorithm"<<endl;
+
+    vector<double>C4_data(9,0);
+    mdspan<double, std::vector<size_t>> C4(C4_data.data(), true, {rows4, cols4});
+
+    strassen_multiply(Q4, R4, C4,par4);
 
 
 
-printmatrix(C4);
-cout<<"or its Winograd variant, with the smaller matrices computed on gpu"<<endl;
+    printmatrix(C4);
+    cout<<"or its Winograd variant, with the smaller matrices computed on gpu"<<endl;
 
-std::fill(C4_data.begin(),C4_data.end(),0);
+    std::fill(C4_data.begin(),C4_data.end(),0);
 
-winograd_multiply(Q4, R4, C4,par4);
+    winograd_multiply(Q4, R4, C4,par4);
 
-printmatrix(C4);
+    printmatrix(C4);
 
 
 }
