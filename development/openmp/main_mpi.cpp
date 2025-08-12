d5b4294ae6cdbd51fa5ce5303a7c64eeba3690da
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2025-08-12 02:26:41 +0200

    Update main_mpi.cpp

--- development/openmp/main_mpi.cpp
+++ development/openmp/main_mpi.cpp
@@ -12,55 +12,56 @@ int main(int argc, char** argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
    size_t rows = 4, cols = 4;
-//demonstraTes MPI send and recieve, works if uncommented
-//    if(process_Rank == 0)
-//    {
-//        int message_type=1;
-//        MPI_Send(&message_type, 1,MPI_INT,1,0, MPI_COMM_WORLD);
-//
-//        vector<double>A_data(16,1);
-//        mdspan<double, std::vector<size_t>> A(A_data.data(), true, {rows, cols});
-//        A.MPI_Send_mdspan_pdata(1,1,MPI_COMM_WORLD);
-//        cout<<"Message Sent:\n";
-//        printmatrix(A);
-//
-//        A.MPI_Recv_mdspan_pdata(1,2,MPI_COMM_WORLD);
-//        printmatrix(A);
-//
-//        vector<double>A2_data(16,2);
-//        mdspan<double, std::vector<size_t>> A2(A2_data.data(), true, {rows, cols});
-//        MPI_send_datastruct(A2.get_datastruct(),1,1,MPI_COMM_WORLD);
-//        cout<<"Message Sent:\n";
-//        printmatrix(A2.get_datastruct());
-//    }
-//
-//    else if(process_Rank == 1)
-//    {
-//        MPI_Status status;
-//        int message_type;
-//        MPI_Recv(&message_type, 1,MPI_INT,0,0, MPI_COMM_WORLD,&status);
-//        vector<double>A2_data(16,2);
-//        mdspan<double, std::vector<size_t>> B(A2_data.data(), true, {rows, cols});
-//        B.MPI_Recv_mdspan_pdata(0,1,MPI_COMM_WORLD);
-//        cout<<"Message recieved";
-//         printmatrix(B);
-//        B.get_datastruct().pdata[0]=42;
-//        B.MPI_Send_mdspan_pdata(status.MPI_SOURCE,2,MPI_COMM_WORLD);
-//
-//        cout<<"Message send:\n";
-//        printmatrix(B);
-//
-//        datastruct<double> dB=MPI_recv_alloc_datastruct<double>(false,false,false,0,0,1,MPI_COMM_WORLD);
-//        cout<<"Message Received\n";
-//        printmatrix(dB);
-//        datastruct_free(dB,false,false,false,0);
-//
-//
-//    }
+//demonstrates recieve and send. Works if uncommented.
+    if(process_Rank == 0)
+    {
+        int message_type=1;
+        MPI_Send(&message_type, 1,MPI_INT,1,0, MPI_COMM_WORLD);
+
+        vector<double>A_data(16,1);
+        mdspan<double, std::vector<size_t>> A(A_data.data(), true, {rows, cols});
+        A.MPI_Send_mdspan_pdata(1,1,MPI_COMM_WORLD);
+        cout<<"Message Sent:\n";
+        printmatrix(A);
+
+        A.MPI_Recv_mdspan_pdata(1,2,MPI_COMM_WORLD);
+        printmatrix(A);
+
+        vector<double>A2_data(16,2);
+        mdspan<double, std::vector<size_t>> A2(A2_data.data(), true, {rows, cols});
+        MPI_send_datastruct(A2.get_datastruct(),1,1,MPI_COMM_WORLD);
+        cout<<"Message Sent:\n";
+        printmatrix(A2.get_datastruct());
+    }
+
+    else if(process_Rank == 1)
+    {
+        MPI_Status status;
+        int message_type;
+        MPI_Recv(&message_type, 1,MPI_INT,0,0, MPI_COMM_WORLD,&status);
+        vector<double>A2_data(16,2);
+        mdspan<double, std::vector<size_t>> B(A2_data.data(), true, {rows, cols});
+        B.MPI_Recv_mdspan_pdata(0,1,MPI_COMM_WORLD);
+        cout<<"Message recieved";
+         printmatrix(B);
+        B.get_datastruct().dpdata[0]=42;
+        B.MPI_Send_mdspan_pdata(status.MPI_SOURCE,2,MPI_COMM_WORLD);
+
+        cout<<"Message send:\n";
+        printmatrix(B);
+
+        datastruct<double> dB=MPI_recv_alloc_datastruct<double>(false,false,false,0,0,1,MPI_COMM_WORLD);
+        cout<<"Message Received\n";
+        printmatrix(dB);
+        datastruct_free(dB,false,false,false,0);
+
+
+    }
 
 //Strassen algorithm does now work with MPI. Be sure to use at least the number of nodes than the recursion needs if you have a single gpu.
-// nvidia unfortunately does not let the programs start several different cuda virtual machines on one device. 
+// nvidia unfortunately does not let the programs start several different cuda virtual machines on one device.
     //probably the algorithms can be optimized further by using MPI_Irecieve/MPI_Isend
+
 
     matrix_multiplication_parameters par;
     par.comm=MPI_COMM_WORLD;
