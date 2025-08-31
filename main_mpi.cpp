377a4a79eed992514bdeeefb271e401606f50089
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2025-08-31 18:54:46 +0200

    Update main_mpi.cpp

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <vector>
-#include "mdspan_omp.h"
+#include "mdspan_data.h"
+#include "mathfunctions.h"
+#include "datastruct_mpifunctions.h"
+#include "mathfunctions_mpi.h"
+
+
 using namespace std;
 
 int main(int argc, char** argv)
@@ -11,93 +16,82 @@ int main(int argc, char** argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
     MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
 
-   size_t rows = 4, cols = 4;
+    size_t rows = 4, cols = 4;
 //demonstrates recieve and send. Works if uncommented.
     if(process_Rank == 0)
     {
-        int message_type=1;
-        MPI_Send(&message_type, 1,MPI_INT,1,0, MPI_COMM_WORLD);
 
-        vector<double>A_data(16,1);
-        mdspan<double, std::vector<size_t>> A(A_data.data(), true, {rows, cols});
-        A.MPI_Send_mdspan_pdata(1,1,MPI_COMM_WORLD);
-        cout<<"Message Sent:\n";
-        printmatrix(A);
 
-        A.MPI_Recv_mdspan_pdata(1,2,MPI_COMM_WORLD);
-        printmatrix(A);
-
-        vector<double>A2_data(16,2);
+        vector<double>A2_data(16,4);
         mdspan<double, std::vector<size_t>> A2(A2_data.data(), true, {rows, cols});
-        MPI_send_datastruct(A2.get_datastruct(),1,1,MPI_COMM_WORLD);
+        Datastruct_MPI_Functions<double>::MPI_Send_datastruct(A2,1,1,MPI_COMM_WORLD);
         cout<<"Message Sent:\n";
-        printmatrix(A2.get_datastruct());
+        A2.printmatrix();
     }
-
+//
     else if(process_Rank == 1)
     {
-        MPI_Status status;
-        int message_type;
-        MPI_Recv(&message_type, 1,MPI_INT,0,0, MPI_COMM_WORLD,&status);
-        vector<double>A2_data(16,2);
-        mdspan<double, std::vector<size_t>> B(A2_data.data(), true, {rows, cols});
-        B.MPI_Recv_mdspan_pdata(0,1,MPI_COMM_WORLD);
+        vector<double>B2_data(16,2);
+        mdspan_data<double, std::vector<size_t>> B(B2_data.data(), true, {rows, cols});
+
+        Datastruct_MPI_Functions<double>::MPI_Recv_datastruct(B,0,1,MPI_COMM_WORLD);
         cout<<"Message recieved";
-         printmatrix(B);
-        B.get_datastruct().dpdata[0]=42;
-        B.MPI_Send_mdspan_pdata(status.MPI_SOURCE,2,MPI_COMM_WORLD);
+        B.printmatrix();
+        B(1,1)=42;
 
-        cout<<"Message send:\n";
-        printmatrix(B);
-
-        datastruct<double> dB=MPI_recv_alloc_datastruct<double>(false,false,false,0,0,1,MPI_COMM_WORLD);
-        cout<<"Message Received\n";
-        printmatrix(dB);
-        datastruct_free(dB,false,false,false,0);
-
-
+//
     }
 
-//Strassen algorithm does now work with MPI. Be sure to use at least the number of nodes than the recursion needs if you have a single gpu.
-// nvidia unfortunately does not let the programs start several different cuda virtual machines on one device.
-    //probably the algorithms can be optimized further by using MPI_Irecieve/MPI_Isend
+//
+ rows = 8, cols = 8;
+////
+//
 
-
-    matrix_multiplication_parameters par;
-    par.comm=MPI_COMM_WORLD;
-    par.mpi=true;
-    par.omp=true;
-    par.gpu_offload=true;
-    par.memmapped_files=true;
-
+    Math_MPI_RecursiveMultiplication_Policy p(Math_Functions_Policy::AUTO,true,true);
+    p.update_host=true;
     if(process_Rank == 0)
     {
-        // Define some data for matrix multiplication A*B=C;
-        vector<double>A3_data(16,0);
-        vector<double>B3_data(16,0);
-        vector<double>C3_data(16,1);
 
-        // Allocate data
+    cout<<"matrix multiplication with the Strassen algorithm over message passing interface. For GPU only, the variable Unified_Shared_Memory should be defined"<<std::endl;
+    cout<<"Note: if GPU_ONLY is set, and Unified_Shared_Memory is not defined, then ordinary matrix multiplication on gpu will be called."<<std::endl;
+    cout<<"in auto mode, the following default treshholds are set in mathfunctions.h and can be changed for convenience"<<std::endl;
+    cout << "max_problem_size_for_gpu;" << "This is the size of the gpu memory, data larger than this is not offloaded"<< std::endl;
+    cout <<" default_cubic_treshold = 256;"<< "The default number of elements at which matrices are auto offloaded in multiplication"<< std::endl;
+    cout<< " default_square_treshold = 1000;"<<"The default number of elements at which matrices are auto offloaded for addition"<< std::endl;
+    cout <<" default_linear_treshold = 1000000;"<<"The default number of elements at which vectors are auto offloaded for addition"<<std::endl;
+
+
+//        // Define some data for matrix multiplication A*B=C;
+        vector<double>A3_data(64,0);
+        vector<double>B3_data(64,0);
+        vector<double>C3_data(64,1);
+////
+////        // Allocate data
         for (size_t i = 0; i < rows * cols; ++i)
         {
             A3_data[i] = i + 1; // Example initialization
             B3_data[i] = i ; // Example initialization
         }
-
+//
         mdspan<double, std::vector<size_t>> A3(A3_data.data(), true, {rows, cols});
         mdspan<double, std::vector<size_t>> B3(B3_data.data(), true, {rows, cols});
         mdspan<double, std::vector<size_t>> C3(C3_data.data(), true, {rows, cols});
+//
+        A3.printmatrix();
+        B3.printmatrix();
 
-        printmatrix(A3);
-        printmatrix(B3);
-        strassen_multiply(A3, B3, C3,par);
-        MPI_recursion_helper_end(MPI_COMM_WORLD);
-        printmatrix(C3);
-   }
+
+
+        Math_Functions_MPI<double>::strassen_multiply(A3, B3, C3,&p);
+        C3.printmatrix();
+        Math_Functions_MPI<double>::MPI_recursion_helper_end(p.comm);
+    }
     else
     {
-        MPI_recursive_multiplication_helper<double>(par);
+        Math_Functions_MPI<double>::MPI_recursive_multiplication_helper(&p);
     }
-   MPI_Finalize();
+
+
+    MPI_Finalize();
     return 0;
 }
