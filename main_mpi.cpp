3109663f5b8053ae32510b7a7db3b3e3069df567
bschulz81 <80183710+bschulz81@users.noreply.github.com>
2025-09-07 02:50:15 +0200

    Add files via upload
    more comments

--- main_mpi.cpp
+++ main_mpi.cpp
@@ -43,47 +43,62 @@ int main(int argc, char** argv)
     }
 
 //
- rows = 8, cols = 8;
+
 ////
 //
-
+    rows = 8, cols = 8;
     Math_MPI_RecursiveMultiplication_Policy p(Math_Functions_Policy::GPU_ONLY,true,true);
     p.update_host=true;
     if(process_Rank == 0)
     {
-
-    cout<<"matrix multiplication with the Strassen algorithm over message passing interface"<<std::endl;
-    cout<<"in auto mode, the following default treshholds are set in mathfunctions.h and can be changed for convenience"<<std::endl;
-    cout << "max_problem_size_for_gpu;" << "This is the size of the gpu memory, data larger than this is not offloaded"<< std::endl;
-    cout <<" default_cubic_treshold = 256;"<< "The default number of elements at which matrices are auto offloaded in multiplication"<< std::endl;
-    cout<< " default_square_treshold = 1000;"<<"The default number of elements at which matrices are auto offloaded for addition"<< std::endl;
-    cout <<" default_linear_treshold = 1000000;"<<"The default number of elements at which vectors are auto offloaded for addition"<<std::endl;
-
-
-//        // Define some data for matrix multiplication A*B=C;
-        vector<double>A3_data(64,0);
-        vector<double>B3_data(64,0);
-        vector<double>C3_data(64,1);
-////
-////        // Allocate data
+        vector<double>A3_data(rows*cols,0);
+        vector<double>B3_data(rows*cols,0);
         for (size_t i = 0; i < rows * cols; ++i)
         {
-            A3_data[i] = i + 1; // Example initialization
-            B3_data[i] = i ; // Example initialization
+            A3_data[i] = i + 1;
+            B3_data[i] = i ;
         }
-//
+
         mdspan<double, std::vector<size_t>> A3(A3_data.data(), true, {rows, cols});
         mdspan<double, std::vector<size_t>> B3(B3_data.data(), true, {rows, cols});
-        mdspan<double, std::vector<size_t>> C3(C3_data.data(), true, {rows, cols});
-//
+
+        cout<<"We define two matrices A and B:" <<endl;
         A3.printmatrix();
         B3.printmatrix();
 
+        {
+            vector<double>C3_data(rows*cols,1);
+            mdspan<double, std::vector<size_t>> C3(C3_data.data(), true, {rows, cols});
 
 
-        Math_Functions_MPI<double>::strassen_multiply(A3, B3, C3,&p);
-        C3.printmatrix();
-        Math_Functions_MPI<double>::MPI_recursion_helper_end(p.comm);
+            cout<<"ordinary matrix multiplication on a single node with openmp. It will decide automatically whether to compute on gpu or not"<<endl;
+
+            Math_Functions_Policy p1(Math_Functions_Policy::AUTO);
+            cout<<"supplying nullptr instead of a pointer to Math_Functions_Policy lets the library use a global default that can be configured."<<endl;
+            vector<double>C_data(rows*cols,1);
+            mdspan<double, std::vector<size_t>> C(C_data.data(), true, {rows, cols});
+            Math_Functions<double>::matrix_multiply_dot(A3, B3, C3,&p1);
+            C3.printmatrix();
+
+        }
+
+        {
+
+            vector<double>C3_data(rows*cols,1);
+            mdspan<double, std::vector<size_t>> C3(C3_data.data(), true, {rows, cols});
+
+
+            cout<<"matrix multiplication with the Strassen algorithm over message passing interface"<<std::endl;
+            cout<<"in auto mode, the following default treshholds are set in mathfunctions.h and can be changed for convenience"<<std::endl;
+            cout << "max_problem_size_for_gpu;" << "This is the size of the gpu memory, data larger than this is not offloaded"<< std::endl;
+            cout <<" default_cubic_treshold = 256;"<< "The default number of elements at which matrices are auto offloaded in multiplication"<< std::endl;
+            cout<< " default_square_treshold = 1000;"<<"The default number of elements at which matrices are auto offloaded for addition"<< std::endl;
+            cout <<" default_linear_treshold = 1000000;"<<"The default number of elements at which vectors are auto offloaded for addition"<<std::endl<<endl;
+
+            Math_Functions_MPI<double>::strassen_multiply(A3, B3, C3,&p);
+            C3.printmatrix();
+            Math_Functions_MPI<double>::MPI_recursion_helper_end(p.comm);
+        }
     }
     else
     {
